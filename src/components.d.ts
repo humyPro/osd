6582d53cfef794317e1b8120f1ff717dacda5657
humin <ab998101>
2024-01-13 22:11:58 +0800

    开始添加表单

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,13 +7,22 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    ElAside: typeof import('element-plus/es')['ElAside']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCol: typeof import('element-plus/es')['ElCol']
+    ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
+    ElForm: typeof import('element-plus/es')['ElForm']
+    ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElInput: typeof import('element-plus/es')['ElInput']
+    ElMain: typeof import('element-plus/es')['ElMain']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElMenuItemGroup: typeof import('element-plus/es')['ElMenuItemGroup']
+    ElOption: typeof import('element-plus/es')['ElOption']
     ElRow: typeof import('element-plus/es')['ElRow']
+    ElSelect: typeof import('element-plus/es')['ElSelect']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     IEpAddHouse: typeof import('~icons/ep/add-house')['default']
     IEpEdit: typeof import('~icons/ep/edit')['default']
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,44 @@
 <template>
   <div class="normal-layout">
     <MenuBar />
+    <div class="main-content">
+      <el-form :inline="false" :model="formInline" class="demo-form-inline">
+        <el-form-item label="Approved by">
+          <el-input v-model="formInline.user" placeholder="Approved by" clearable />
+        </el-form-item>
+        <el-form-item label="Activity zone">
+          <el-select v-model="formInline.region" placeholder="Activity zone" clearable>
+            <el-option label="Zone one" value="shanghai" />
+            <el-option label="Zone two" value="beijing" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="Activity time">
+          <el-date-picker
+            v-model="formInline.date"
+            type="date"
+            placeholder="Pick a date"
+            clearable
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">Query</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
-<script lang="ts">
+<script lang="ts" setup>
 import MenuBar from '@/components/MenuBar.vue'
-export default {}
+import { reactive } from 'vue'
+
+const formInline = reactive({
+  user: '',
+  region: '',
+  date: ''
+})
+
+const onSubmit = () => {
+  console.log('submit!')
+}
 </script>
 <style scoped></style>
