efbc5225cdd6cd2c03a65de316f3cf931373db19
humin <ab998101>
2024-01-20 14:31:44 +0800

    souce and network

--- src/views/NetworkView.vue
+++ src/views/NetworkView.vue
@@ -1,6 +1,50 @@
 <template>
   <div class="normal-layout">
     <MenuBar />
-    <h3>网络设置页面开发中</h3>
+    <div class="main-content">
+      <div class="forms-box single-forms">
+        <div class="form-box bordered" style="width: 300px">
+          <FormTitle title="有线网络参数"></FormTitle>
+          <el-form
+            :inline="true"
+            :model="formInline"
+            class="inline-form"
+            label-position="left"
+            label-width="100px"
+          >
+            <el-form-item label="IP地址" class="custom-select">
+              <el-input />
+            </el-form-item>
+            <el-form-item label="子网掩码" class="custom-select">
+              <el-input />
+            </el-form-item>
+            <el-form-item label="默认网关" class="custom-select">
+              <el-input />
+            </el-form-item>
+            <el-form-item label="DNS1" class="custom-select">
+              <el-input />
+            </el-form-item>
+            <el-form-item label="DHCP" label-width="120px">
+              <el-switch v-model="active" />
+            </el-form-item>
+            <el-form-item class="form-right-button">
+              <el-button type="primary">保存</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      sourceConfigs: {},
+      formInline: {},
+      active: true
+    }
+  }
+}
+</script>
+<style></style>
--- src/views/RtmpView.vue
+++ src/views/RtmpView.vue
@@ -1,6 +1,49 @@
 <template>
   <div class="normal-layout">
     <MenuBar />
-    <h3>推流设置页面开发中</h3>
+    <div class="main-content">
+      <div class="forms-box single-forms">
+        <div
+          class="form-box bordered"
+          v-for="(rtmpConfig, index) in rtmpConfigs"
+          :key="index.toString()"
+        >
+          <FormTitle title="编码通道1"></FormTitle>
+          <el-form
+            :inline="true"
+            :model="formInline"
+            class="inline-form"
+            label-position="left"
+            label-width="100px"
+          >
+            <div>
+              <el-form-item label="RTMP 推流地址" label-width="120px">
+                <el-switch v-model="active" />
+              </el-form-item>
+            </div>
+            <div>
+              <el-form-item :style="{ width: '600px' }">
+                <el-input></el-input>
+              </el-form-item>
+            </div>
+            <el-form-item class="form-right-button">
+              <el-button type="primary">保存</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      rtmpConfigs: [{}, {}],
+      formInline: {},
+      active: true
+    }
+  }
+}
+</script>
+<style></style>
