666015ef67fc793628b5315d10ed8c525f82e1d3
humin <ab998101>
2024-02-03 11:01:46 +0800

    xmlToJson

--- src/common/apis/index.ts
+++ src/common/apis/index.ts
@@ -1,22 +1,26 @@
 import type { EncodingForm } from './modelTypes'
 import confis from '../configs'
 
-type methodType = 'get' | 'post' | 'put' | 'delete'
-
-const request = <T>(params: {
-  url: string
-  method: methodType
-  timeout?: number
-  respParser?: (resp: Response) => Promise<T>
-}): Promise<T> => {
+type RequestType<T> = {
+  url: string // 请求地址
+  method: 'get' | 'post' | 'put' | 'delete'
+  timeout?: number //超时时间
+  respParser?: (resp: Response) => Promise<T> // 自定义的response解析
+}
+/**
+ *  通用request方法
+ * @param params
+ * @returns
+ */
+const request = async <T>(params: RequestType<T>): Promise<T> => {
   const { url, method, timeout, respParser } = params
   const init: RequestInit = {
     credentials: 'same-origin',
     method
   }
-  if (timeout !== undefined) {
+  if (timeout) {
     let abort: (() => void) | null = null
-    const abortPromise = new Promise((resolve, reject) => {
+    const abortPromise = new Promise((_resolve, reject) => {
       abort = () => reject(new Error('请求超时'))
     })
     const result = Promise.race([
@@ -39,6 +43,32 @@ const request = <T>(params: {
     return response.json() as Promise<T>
   })
 }
+/**
+ * XML转换为json
+ * @param xmlString
+ * @returns
+ */
+const xmlToJson = (xmlString: string) => {
+  const parser = new DOMParser()
+  const xmlDoc = parser.parseFromString(xmlString, 'text/xml')
+  const json: { [key: string]: any } = {}
+  const parseElement = function (element: Node) {
+    if (element.hasChildNodes()) {
+      for (let i = 0; i < element.childNodes.length; i++) {
+        const child = element.childNodes[i]
+        if (child.nodeType === 1) {
+          if (child.hasChildNodes()) {
+            json[child.nodeName] = parseElement(child)
+          } else {
+            json[child.nodeName] = child.textContent
+          }
+        }
+      }
+    }
+  }
+  parseElement(xmlDoc)
+  return json
+}
 
 const getEncodingForm: () => Promise<EncodingForm> = () => {
   return request({
@@ -50,4 +80,6 @@ const getEncodingForm: () => Promise<EncodingForm> = () => {
   })
 }
 
-getEncodingForm()
+export default {
+  getEncodingForm
+}
