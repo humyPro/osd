094da738ff7998598f3e941717e2ac4ff3e10659
humin <ab998101>
2024-02-20 15:12:51 +0800

    视频设置页面

--- src/common/apis/modelTypes/index.ts
+++ src/common/apis/modelTypes/index.ts
@@ -12,4 +12,44 @@ type EncodingForm = {
   urls: string[]
 }
 
-export type { EncodingForm }
+type UDPForm = {
+  model: string //模式
+  localPort: number //本机端口
+  targetIp: string //目的IP
+  targetPort: number //目的端口
+  sendLength: number //发送长度
+  sendPeriod: number //发送周期
+}
+type RTSPForm = {
+  serverPort: number //服务器端口
+  transferModel: 'UDP' | 'TCP' //传输模式
+  streamName: string //流名称
+  userNumber: number //用户数
+  keepAlive: number //保活时间
+}
+type RTMPForm = {
+  streamAddress: string //推流地址
+}
+type GB28181Form = {
+  sipServerAddres: string //SIP服务器地址
+  sipServerPort: number //SIP服务器地址
+  sipServerId: string //SIP服务器ID
+  registerTerm: number //注册有效期
+  heartBeatPeriod: number //心跳周期
+  sipLocalPort: number //本地SIP端口
+  sipAuthId: string //SIP认证ID
+  sipAuthPassword: string //SIP认证密码
+}
+type VideoForm = {
+  udpH26XEnable: boolean //是否启用UDP H26X
+  udpH26XInfo: UDPForm
+  udpTsEnable: boolean //是否启用UDP TS
+  udpTsInfo: UDPForm
+  rtspEnable: boolean //是否启用RTSP
+  rtspInfo: RTSPForm
+  rtmpEnable: boolean //是否启用rtmp
+  rtmpInfo: RTMPForm
+  gb28181Enable: boolean //是否启用GB28181
+  gb28181Info: GB28181Form
+}
+export type { EncodingForm, UDPForm, VideoForm, RTSPForm, RTMPForm, GB28181Form }
--- src/views/VideoView.vue
+++ src/views/VideoView.vue
@@ -3,155 +3,167 @@
     <MenuBar />
     <div class="main-content">
       <div class="forms-box">
-        <div class="form-box bordered" v-for="video in videos" :key="video.index">
+        <div class="form-box bordered" v-for="(video, index) in videos" :key="index">
           <div class="title" style="margin-bottom: 10px">
-            <FormTitle :title="'视频' + video.index + '参数设置'" text-type="primary" />
+            <FormTitle :title="'视频' + index + '参数设置'" text-type="primary" />
           </div>
-          <el-form :model="formInline" label-position="left" :inline="false">
+          <el-form :model="video" label-position="left" :inline="false">
             <el-form-item label="UDP H26X" class="dot-left">
-              <el-switch v-model="active" />
+              <el-switch v-model="video.udpH26XEnable" />
             </el-form-item>
-            <el-form-item label="模式">
-              <el-select v-model="formInline.region" placeholder="">
-                <el-option label="单播" value="单播" />
-                <el-option label="组播" value="组播" />
-                <el-option label="广播" value="广播" />
-              </el-select>
-            </el-form-item>
-            <el-form-item label="本机端口">
-              <el-input />
-            </el-form-item>
-            <el-form-item label="目的IP">
-              <el-input placeholder="ip" />
-            </el-form-item>
-            <el-form-item label="目的端口">
-              <el-input />
-            </el-form-item>
-            <el-form-item label="发送长度">
-              <el-input />
-            </el-form-item>
-            <el-form-item label="发送间隔">
-              <el-input />
-            </el-form-item>
+            <template v-if="video.udpH26XEnable">
+              <el-form-item label="模式">
+                <el-select v-model="video.udpH26XInfo.model" placeholder="">
+                  <el-option label="单播" value="单播" />
+                  <el-option label="组播" value="组播" />
+                  <el-option label="广播" value="广播" />
+                </el-select>
+              </el-form-item>
+              <el-form-item label="本机端口">
+                <el-input v-model.number="video.udpH26XInfo.localPort" placeholder="[0-65535]" />
+              </el-form-item>
+              <el-form-item label="目的IP">
+                <el-input placeholder="ip" v-model="video.udpH26XInfo.targetIp" />
+              </el-form-item>
+              <el-form-item label="目的端口">
+                <el-input placeholder="[0-65535]" v-model.number="video.udpH26XInfo.targetPort" />
+              </el-form-item>
+              <el-form-item label="发送长度">
+                <el-input v-model.number="video.udpH26XInfo.sendLength" />
+              </el-form-item>
+              <el-form-item label="发送间隔">
+                <el-input v-model="video.udpH26XInfo.sendPeriod" />
+              </el-form-item>
+            </template>
             <el-divider />
             <el-form-item label="UDP TS" class="dot-left">
-              <el-switch v-model="active" />
+              <el-switch v-model="video.udpTsEnable" />
             </el-form-item>
-            <el-form-item label="模式">
-              <el-select v-model="formInline.region" placeholder="">
-                <el-option label="单播" value="单播" />
-                <el-option label="组播" value="组播" />
-                <el-option label="广播" value="广播" />
-              </el-select>
-            </el-form-item>
-            <el-form-item label="本机端口">
-              <el-input />
-            </el-form-item>
-            <el-form-item label="目的IP">
-              <el-input placeholder="ip" />
-            </el-form-item>
-            <el-form-item label="目的端口">
-              <el-input />
-            </el-form-item>
-            <el-form-item label="发送长度">
-              <el-input />
-            </el-form-item>
-            <el-form-item label="发送间隔">
-              <el-input />
-            </el-form-item>
+            <template v-if="video.udpTsEnable">
+              <el-form-item label="模式">
+                <el-select v-model="video.udpTsInfo.model" placeholder="">
+                  <el-option label="单播" value="单播" />
+                  <el-option label="组播" value="组播" />
+                  <el-option label="广播" value="广播" />
+                </el-select>
+              </el-form-item>
+              <el-form-item label="本机端口">
+                <el-input v-model.number="video.udpTsInfo.localPort" placeholder="[0-65535]" />
+              </el-form-item>
+              <el-form-item label="目的IP">
+                <el-input placeholder="ip" v-model="video.udpTsInfo.targetIp" />
+              </el-form-item>
+              <el-form-item label="目的端口">
+                <el-input v-model.number="video.udpTsInfo.targetPort" placeholder="[0-65535]" />
+              </el-form-item>
+              <el-form-item label="发送长度">
+                <el-input v-model.number="video.udpTsInfo.sendLength" />
+              </el-form-item>
+              <el-form-item label="发送间隔">
+                <el-input v-model.number="video.udpTsInfo.sendPeriod" />
+              </el-form-item>
+            </template>
             <el-divider />
             <el-form-item label="RTSP" class="dot-left">
-              <el-switch v-model="active" />
+              <el-switch v-model="video.rtspEnable" />
             </el-form-item>
-            <el-form-item label="服务端口">
-              <el-input />
-            </el-form-item>
-            <el-form-item label="传输模式">
-              <el-select v-model="formInline.region" placeholder="">
-                <el-option label="UDP" value="UDP" />
-                <el-option label="TCP" value="TCP" />
-              </el-select>
-            </el-form-item>
-            <el-form-item label="流名称">
-              <el-input placeholder="[格式：/stream0,重启生效]" />
-            </el-form-item>
-            <el-form-item label="用户数">
-              <el-input placeholder="[1~5]" />
-            </el-form-item>
-            <el-form-item label="保活时间(ms)">
-              <el-input placeholder="[毫秒]" />
-            </el-form-item>
-            <el-divider />
-            <el-form-item label="RTMP" class="dot-left">
-              <el-switch v-model="active" />
-            </el-form-item>
-            <el-form-item label="推流地址">
-              <el-input />
-            </el-form-item>
+            <template v-if="video.rtspEnable">
+              <el-form-item label="服务端口">
+                <el-input v-model.number="video.rtspInfo.serverPort" placeholder="[0-65535]" />
+              </el-form-item>
+              <el-form-item label="传输模式">
+                <el-select v-model="video.rtspInfo.transferModel" placeholder="">
+                  <el-option label="UDP" value="UDP" />
+                  <el-option label="TCP" value="TCP" />
+                </el-select>
+              </el-form-item>
+              <el-form-item label="流名称">
+                <el-input
+                  placeholder="[格式：/stream0,重启生效]"
+                  v-model="video.rtspInfo.streamName"
+                />
+              </el-form-item>
+              <el-form-item label="用户数">
+                <el-input placeholder="[1~5]" v-model.number="video.rtspInfo.userNumber" />
+              </el-form-item>
+              <el-form-item label="保活时间(ms)">
+                <el-input placeholder="[毫秒]" v-model.number="video.rtspInfo.keepAlive" />
+              </el-form-item>
+              <el-divider />
+              <el-form-item label="RTMP" class="dot-left">
+                <el-switch v-model="video.rtmpEnable" />
+              </el-form-item>
+              <el-form-item label="推流地址">
+                <el-input v-model="video.rtmpInfo.streamAddress" />
+              </el-form-item>
+            </template>
             <el-divider />
             <el-form-item label="GB28181" class="dot-left">
-              <el-switch v-model="active" />
+              <el-switch v-model="video.gb28181Enable" />
             </el-form-item>
-            <el-form-item label="SIP服务器地址">
-              <el-input />
-            </el-form-item>
-            <el-form-item label="SIP服务器端口">
-              <el-input />
-            </el-form-item>
-            <el-form-item label="SIP服务器ID">
-              <el-input />
-            </el-form-item>
-            <el-form-item label="注册有效期">
-              <el-input />
-            </el-form-item>
-            <el-form-item label="心跳周期">
-              <el-input />
-            </el-form-item>
-            <el-form-item label="本地SIP端口">
-              <el-input />
-            </el-form-item>
-            <el-form-item label="SIP认证ID">
-              <el-input />
-            </el-form-item>
-            <el-form-item label="SIP认证密码">
-              <el-input />
-            </el-form-item>
+            <template v-if="video.gb28181Enable">
+              <el-form-item label="SIP服务器地址">
+                <el-input v-model="video.gb28181Info.sipServerAddres" />
+              </el-form-item>
+              <el-form-item label="SIP服务器端口">
+                <el-input
+                  v-model.number="video.gb28181Info.sipServerPort"
+                  placeholder="[0-65535]"
+                />
+              </el-form-item>
+              <el-form-item label="SIP服务器ID">
+                <el-input v-model="video.gb28181Info.sipServerId" />
+              </el-form-item>
+              <el-form-item label="注册有效期">
+                <el-input v-model="video.gb28181Info.registerTerm" />
+              </el-form-item>
+              <el-form-item label="心跳周期">
+                <el-input v-model.number="video.gb28181Info.heartBeatPeriod" />
+              </el-form-item>
+              <el-form-item label="本地SIP端口">
+                <el-input v-model.number="video.gb28181Info.sipLocalPort" placeholder="[0-65535]" />
+              </el-form-item>
+              <el-form-item label="SIP认证ID">
+                <el-input v-model="video.gb28181Info.sipAuthId" />
+              </el-form-item>
+              <el-form-item label="SIP认证密码">
+                <el-input v-model="video.gb28181Info.sipAuthPassword" />
+              </el-form-item>
+            </template>
+            <el-button class="save-button" type="primary">保存</el-button>
           </el-form>
-          <el-button class="fixed-button" type="primary">保存</el-button>
         </div>
       </div>
     </div>
   </div>
 </template>
-<script lang="ts">
+<script lang="ts" setup>
+import type { RTMPForm, RTSPForm, UDPForm, VideoForm, GB28181Form } from '@/common/apis/modelTypes'
 import MenuBar from '@/components/MenuBar.vue'
-export default {
-  components: {
-    MenuBar
-  },
-  data() {
-    return {
-      formInline: {
-        region: ''
-      },
-      videos: [
-        {
-          index: 1
-        },
-        {
-          index: 2
-        },
-        {
-          index: 3
-        }
-      ],
-      QRCode: ''
-    }
-  },
-  methods: {
-    onSubmit() {}
-  }
+import { ref } from 'vue'
+const defaultForm = {
+  udpH26XEnable: false,
+  udpH26XInfo: {} as UDPForm,
+  udpTsEnable: false,
+  udpTsInfo: {} as UDPForm,
+  rtspEnable: false,
+  rtspInfo: {} as RTSPForm,
+  rtmpEnable: false,
+  rtmpInfo: {} as RTMPForm,
+  gb28181Enable: false,
+  gb28181Info: {} as GB28181Form
 }
+const videos = ref<VideoForm[]>([
+  {
+    ...defaultForm
+  },
+  {
+    ...defaultForm
+  },
+  {
+    ...defaultForm
+  }
+])
 </script>
 <style scoped>
 .forms-box {
